0f9206a8afded120aadfbb077f040b4340d542ff
Athmajan A S <73539585+Athmajanaji@users.noreply.github.com>
2021-12-15 09:35:52 +0530

    prime

---
+++ prime.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number:"))
+c=0
+if a%1==0 and a%a==0:
+    for i in range(2,a):
+        if a%i==0:
+            c+=1
+if(c==0):
+    print("prime")
+else:
+    print("Not prime")
