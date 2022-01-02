9bed9d8335889cd909c6bbd5bb7828c698cdf395
Athmajan A S <73539585+Athmajanaji@users.noreply.github.com>
2022-01-02 20:16:43 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,11 +1,5 @@
 a=int(input("Enter first number"))
-if a%4==0:
-    if a%100==0:
-        if a%400==0:
-            print(a," leap year")
-        else:
-            print("not a leap year")
-    else:
-       print("not a leap year")
+if a%4==0 or a%400 and a%100!=0:
+    print("leap year")
 else:
     print("not a leap year")
