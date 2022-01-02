3834736d20b5b44c6c9aea46fffa733638477582
Athmajan A S <73539585+Athmajanaji@users.noreply.github.com>
2022-01-02 20:32:00 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -4,7 +4,9 @@ if a%1==0 and a%a==0:
     for i in range(2,a):
         if a%i==0:
             c+=1
-if(c==0):
+if a==1:
+    print("neither prime nor composite")
+elif(c==0):
     print("prime")
 else:
     print("Not prime")
